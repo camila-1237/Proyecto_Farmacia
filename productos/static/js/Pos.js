92b6990b4c9e2d14f813cc356e69dc256c857210
Camila Machado <machado.camila@iemodeliaibague.edu.co>
2023-12-06 08:55:58 -0500

    cambios

--- productos/static/js/Pos.js
+++ productos/static/js/Pos.js
@@ -1,21 +1,23 @@
-const listaProductosVenta = [];
+const listaProductosVenta = []
 
 document.addEventListener('DOMContentLoaded', function() {
     const buscador = document.getElementById('buscador');
     const listaProductos = document.getElementById('lista-productos');
-    const productos = document.querySelectorAll(".producto");
+    const productos = document.querySelectorAll(".producto")
     const editarModalBtn = document.getElementById('editar-modal');
 
     const modalNombre = document.getElementById('modal-nombre');
     const modalPrecio = document.getElementById('modal-precio');
     const modalCantidad = document.getElementById('modal-cantidad');
-    const valorPago = document.getElementById("pago");
+    const valorPago = document.getElementById("pago")
     const agregarModalBtn = document.getElementById('agregar-modal');
 
-    const realizarVenta = document.getElementById("finalizar");
+    const realizarVenta = document.getElementById("finalizar")
 
-    const totalTodo = document.getElementById("transaccion");
-    const prds = document.getElementById("total");
+    const totalTodo = document.getElementById("transaccion")
+    const prds = document.getElementById("total")
+
+    let productoEditado = null;
 
     buscador.addEventListener('input', function() {
         const filtro = this.value.toLowerCase();
@@ -32,7 +34,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     productos.forEach(prd => {
+
         prd.addEventListener("click", () => {
+
             const nombre = prd.textContent.split(' - ')[0];
             const precio = parseFloat(prd.dataset.precio);
 
@@ -44,70 +48,70 @@ document.addEventListener('DOMContentLoaded', function() {
 
             const modal = document.getElementById('modal');
             modal.style.display = 'block';
-        });
-    });
 
+        })
+
+    })
+
+    function borrar(id){
+
+        const lista = JSON.parse(localStorage.getItem("productos"))
+
+        const nuevaLista = lista.slice(id, 1)
+    
+        console.log(id)
+
+    }
     agregarModalBtn.addEventListener('click', () => {
         const cantidad = parseInt(modalCantidad.value);
-
+    
         if (cantidad > 0) {
             modal.style.display = 'none';
             modalCantidad.value = '1';
-
+    
             const nombreConvertido = modalNombre.textContent.trim();
-            const precioConvertido = modalPrecio.textContent.split("$");
-
-            listaProductosVenta.push({
-                nombre: nombreConvertido,
-                cantidad: cantidad,
-                precio: parseFloat(precioConvertido[1]) * cantidad
-            });
-
-            localStorage.setItem("productos", JSON.stringify(listaProductosVenta));
-
-            let productos = JSON.parse(localStorage.getItem("productos"));
-
-            let total = 0;
-
-            prds.innerHTML = "<p> </p>";
-
-            for (let i in productos) {
-                prds.innerHTML += `
-                    <span> Nombre: ${productos[i].nombre} - Cantidad: ${productos[i].cantidad} </span>
-                    <button class="eliminar-producto" data-id="${i}"> X </button> <br>
-                `
-                total += productos[i].precio
+            const precioTexto = modalPrecio.textContent.split("$")[1].trim(); // Obtener el texto del precio
+    
+            // Limpiar el valor para asegurarse de que sea un número válido
+            const precioNumerico = parseFloat(precioTexto.replace(/[^\d.]/g, '').replace(',', '.'));
+    
+            // Verificar si el precio es un número antes de realizar cálculos
+            if (!isNaN(precioNumerico)) {
+                const precioTotal = cantidad * precioNumerico;
+    
+                listaProductosVenta.push({
+                    nombre: nombreConvertido,
+                    cantidad: cantidad,
+                    precio: precioTotal
+                });
+    
+                localStorage.setItem("productos", JSON.stringify(listaProductosVenta));
+    
+                let productos = JSON.parse(localStorage.getItem("productos"));
+    
+                let total = 0;
+    
+                prds.innerHTML = "<p> </p>";
+    
+                for (let i in productos) {
+                    prds.innerHTML += `
+                        <span> Nombre: ${productos[i].nombre} - Cantidad: ${productos[i].cantidad} </span>
+                        <button class="eliminar-producto" data-id="${i}"> X </button> <br>
+                    `;
+                    total += productos[i].precio;
+                }
+    
+                if (!isNaN(total)) { // Verificar si el total es un número válido
+                    totalTodo.textContent = `Total: $${total.toFixed(2)}`;
+                } else {
+                    alert("Error: El total no es un número válido");
+                }
+            } else {
+                alert("Error: El precio no es un número válido");
             }
-
-            totalTodo.textContent = `Total: $${total.toFixed(2)}`;
         }
     });
-
-    prds.addEventListener('click', (e) => {
-        if (e.target.classList.contains('eliminar-producto')) {
-            const index = parseInt(e.target.dataset.id);
-            listaProductosVenta.splice(index, 1);
-
-            localStorage.setItem("productos", JSON.stringify(listaProductosVenta));
-
-            let productos = JSON.parse(localStorage.getItem("productos"));
-
-            let total = 0;
-
-            prds.innerHTML = "<p> </p>";
-
-            for (let i in productos) {
-                prds.innerHTML += `
-                    <span> Nombre: ${productos[i].nombre} - Cantidad: ${productos[i].cantidad} </span>
-                    <button class="eliminar-producto" data-id="${i}"> X </button> <br>
-                `
-                total += productos[i].precio
-            }
-
-            totalTodo.textContent = `Total: $${total.toFixed(2)}`;
-        }
-    });
-
+    
     const cantidadSelector = document.querySelector('.cantidad-selector');
     cantidadSelector.addEventListener('click', function(event) {
         if (event.target.classList.contains('cantidad-btn')) {
